beb4958ae5ac0c9a9748915056dbb785b2c2b374
devayush-bajaj-coditas <devayush.bajaj@coditas.com>
2022-08-11 17:58:25 +0100

    Adding file

---
+++ src/Week_4/Day17/Q2_Employee_Input_All_Ways/Console_Input.java
@@ -0,0 +1,14 @@
+package Week_4.Day17.Q2_Employee_Input_All_Ways;
+import java.io.Console;
+public class Console_Input {
+	public static void main(String[] args) {
+		EmployeeDetails employeeDetails = new EmployeeDetails();
+		Console con = System.console();
+		employeeDetails.employeeName = con.readLine();
+		employeeDetails.employeeId = Integer.parseInt(con.readLine());
+		employeeDetails.employeeSalary = Float.parseFloat(con.readLine());
+		System.out.println("Name : " + employeeDetails.employeeName+ "\nID : " + employeeDetails.employeeId +"\nSalary : " + employeeDetails.employeeSalary );
+
+	}
+
+}
