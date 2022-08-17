6a4960c25f56b2059220d5c85fa501e4a7504f9e
devayush-bajaj-coditas <devayush.bajaj@coditas.com>
2022-08-17 20:05:49 +0100

    Adding file

--- src/Week_4/Day17/Q2_Employee_Input_All_Ways/Console_Input.java
+++ src/Week_4/Day17/Q2_Employee_Input_All_Ways/Console_Input.java
@@ -1,5 +1,6 @@
 package Week_4.Day17.Q2_Employee_Input_All_Ways;
 import java.io.Console;
+import Week_4.Day17.Q2_Employee_Input_All_Ways.EmployeeDetails;
 public class Console_Input {
 	public static void main(String[] args) {
 		EmployeeDetails employeeDetails = new EmployeeDetails();
